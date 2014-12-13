f4d6b249d642c4f3946c44f02b5c0693b646bb77
astrapi69 <asterios.raptis@gmx.net>
2014-12-13 13:25:12 +0100

    new behavior for timeline.

---
+++ js-integration/src/main/java/de/alpharogroup/googlecharts/timeline/TimelineBehavior.java
@@ -0,0 +1,12 @@
+package de.alpharogroup.googlecharts.timeline;
+
+import org.apache.wicket.behavior.Behavior;
+
+public class TimelineBehavior extends Behavior  {
+
+	/**
+	 * The serialVersionUID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
