8f91e9b4f8b3588449e70496e2bb16ca32ca0f05
astrapi69 <asterios.raptis@gmx.net>
2015-02-28 20:33:38 +0100

    Added an employee panel

--- rest-annotations/src/main/java/org/rest/annotations/HomePage.java
+++ rest-annotations/src/main/java/org/rest/annotations/HomePage.java
@@ -1,12 +1,18 @@
 package org.rest.annotations;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.jaulp.test.objects.Employee;
+import org.rest.annotations.panels.EmployeePanel;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+
+		add(new EmployeePanel("employeePanel", Model.of(Employee.builder().build())));
+
     }
 }
--- rest-annotations/src/main/java/org/rest/annotations/resources/EmployeeValidator.java
+++ rest-annotations/src/main/java/org/rest/annotations/resources/EmployeeValidator.java
@@ -1,17 +1,30 @@
 package org.rest.annotations.resources;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidator;
 import org.jaulp.test.objects.Employee;
+import org.wicketstuff.rest.utils.wicket.validator.RestValidationError;
 
 public class EmployeeValidator implements IValidator<Employee>
 {
+
+	/**
+	 * The serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void validate(IValidatable<Employee> validatable)
 	{
 		Employee employee = validatable.getValue();
-		// do validation...
+		if(employee.getPerson().getName() == null) {
+            validatable.error(
+            	new RestValidationError(Arrays.asList("no.name"),
+            		 Collections.<String, Object> emptyMap(), "name"));
+		}
 	}
 
 }
