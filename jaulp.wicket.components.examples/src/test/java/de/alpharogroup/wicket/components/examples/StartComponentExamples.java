deb1df87812964a94b63acd64d8449d3e3895484
Asterios Raptis <asterios.raptis@gmx.net>
2017-03-26 16:21:19 +0200

    optimise start class

--- jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/StartComponentExamples.java
+++ jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/StartComponentExamples.java
@@ -16,6 +16,8 @@
 package de.alpharogroup.wicket.components.examples;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.apache.wicket.util.time.Duration;
@@ -25,7 +27,8 @@ import de.alpharogroup.jetty9.runner.Jetty9Runner;
 import de.alpharogroup.jetty9.runner.WicketJetty9Runner;
 import de.alpharogroup.jetty9.runner.config.StartConfig;
 import de.alpharogroup.log.LoggerExtensions;
-import de.alpharogroup.wicket.base.application.ConfigurationPropertiesResolver;
+import de.alpharogroup.resourcebundle.config.ConfigurationPropertiesResolver;
+import de.alpharogroup.resourcebundle.properties.PropertiesExtensions;
 import de.alpharogroup.wicket.components.examples.application.WicketApplication;
 import lombok.experimental.ExtensionMethod;
 
@@ -45,7 +48,14 @@ public class StartComponentExamples
 
 	private static void startWicketApplication(final ConfigurationPropertiesResolver configurationPropertiesResolver)
 	{
-		final String projectName = "jaulp.wicket.components.examples";
+		final int sessionTimeout = (int) Duration.minutes(30).seconds();// set timeout to 30min(60sec * 30min=1800sec)...
+
+		String configurationType;
+		// comment development and uncomment deployment if you want to run in deployment mode
+		configurationType = "development";
+		// configurationType = "deployment";
+		System.setProperty("wicket.configuration", configurationType);
+		final String projectName = getProjectNameQuietly("jaulp.wicket.components.examples");
 		final File projectDirectory = PathFinder.getProjectDirectory();
 		final File webapp = Jetty9Runner.getWebappDirectory(projectDirectory, projectName);
 		final File logFile = Jetty9Runner.getLogFile(projectDirectory, "application.log");
@@ -59,14 +69,64 @@ public class StartComponentExamples
 			.keyStorePathResource("/keystore")
 			.projectDirectory(projectDirectory)
 			.projectName(projectName)
-			.runtimeConfigurationType("deployment")
-			.sessionTimeout((int)Duration.minutes(1).seconds())
+			.runtimeConfigurationType(configurationType)
+			.sessionTimeout(sessionTimeout)
 			.webapp(webapp)
 			.logFile(logFile)
 			.absolutePathFromLogfile(logFile.getAbsolutePath())
 			.build();
 
 		WicketJetty9Runner.run(startConfig);
+	}
+
+	/**
+	 * Gets the project name from the 'project.properties'. In this properties file is only a
+	 * reference of the artifactId from the pom.
+	 *
+	 * @return the project name
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	protected static String getProjectName() throws IOException
+	{
+		final Properties projectProperties = PropertiesExtensions
+			.loadProperties("project.properties");
+		if (projectProperties != null)
+		{
+			final String projectName = projectProperties.getProperty("artifactId");
+			if (projectName == null)
+			{
+				throw new RuntimeException(
+					"No properties key 'artifactId' found in the properties file project.properties exist.");
+			}
+			return projectName;
+		}
+		else
+		{
+			throw new RuntimeException("No properties file project.properties exist.");
+		}
+	}
+
+	/**
+	 * Gets the project name from the 'project.properties'. In this properties file is only a
+	 * reference of the artifactId from the pom.
+	 *
+	 * @param defaultName
+	 *            the default project name if
+	 * @return the project name
+	 */
+	protected static String getProjectNameQuietly(final String defaultName)
+	{
+		try
+		{
+			getProjectName();
+		}
+		catch (final Exception e)
+		{
+			// default project name will be returned...
+			e.printStackTrace();
+		}
+		return defaultName;
 	}
 
 }
